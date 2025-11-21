72f867329ebcd5698bd19e6d97f57213390bbf92
SaranyaCP-GitHub <saranya.cp@techjays.com>
2025-11-21 15:50:23 +0530

    Initial commit: Django starter project

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_9rmcEbyIjn1X',
+        'HOST': 'ep-sweet-sky-afjms6o2-pooler.c2.us-west-2.aws.neon.tech',
+        'PORT': '5432',
+    }
+}
